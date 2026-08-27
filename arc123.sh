d098ab7e40430e4e8cbfb715ca4a41ca9d60a482
Ravi Arnan <raviarnankeren@gmail.com>
2026-08-27 11:42:30 +0800

    arc123: try direct fields format for metadata template

--- arc123.sh
+++ arc123.sh
@@ -102,15 +102,13 @@ ASPECT_TYPE_ID=$(echo "$ASPECT_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
 if gcloud dataplex aspect-types describe "$ASPECT_TYPE_ID" --location="$REGION" --project="$PROJECT_ID" >/dev/null 2>&1; then
   echo "Aspect type $ASPECT_TYPE_ID sudah ada, lewati create"
 else
-  # Create metadata template file - correct format for Dataplex aspect types
+  # Create metadata template file - try direct fields format
   cat > /tmp/metadata_template.json <<EOF
 {
-  "metadataTemplate": {
-    "fields": [
-      {"name": "has_sensitive_data", "type": "BOOLEAN", "displayName": "Has Sensitive Data"},
-      {"name": "sensitive_data_type", "type": "ENUM", "displayName": "Sensitive Data Type", "enumValues": ["Location Info", "Contact Info", "None"]}
-    ]
-  }
+  "fields": [
+    {"name": "has_sensitive_data", "type": "BOOLEAN", "displayName": "Has Sensitive Data"},
+    {"name": "sensitive_data_type", "type": "ENUM", "displayName": "Sensitive Data Type", "enumValues": ["Location Info", "Contact Info", "None"]}
+  ]
 }
 EOF
   gcloud dataplex aspect-types create "$ASPECT_TYPE_ID" \
